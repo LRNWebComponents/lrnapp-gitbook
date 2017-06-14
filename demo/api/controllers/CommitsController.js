d4f4ec90f0f9f8493341eea13d809dde9503582a
heyMP <mgp140@gmail.com>
2017-06-14 16:44:09 -0400

    updated demo

--- demo/api/controllers/CommitsController.js
+++ demo/api/controllers/CommitsController.js
@@ -5,12 +5,28 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 var shell = require('shelljs');
+var Rx = require('rxjs/Rx');
 
 module.exports = {
   'all': function(req,res) {
     // get branches
-    var branches = shell.exec('git branch -l');
-    var status = shell.exec('git log master --format=\'{"refs": "%d", "commit": "%h", "tree": "%t", "parent": "%p", "subject": "%s", "date": "%cd", "author": "%an %ae"},\' --reverse');
-    res.json(branches);
+    var output = [];
+    var branches = ['master','test'];
+    branches.forEach(function(b,i) {
+      var logCmd = 'git log '+ b +' --format=\'{"refs": "%d", "commit": "%h", "tree": "%t", "parent": "%p", "subject": "%s", "date": "%cd", "author": "%an %ae"},\' --reverse';
+      var _commits = shell.exec(logCmd);
+      var _commits = _commits.slice(0, -1);
+      var _commits = _commits.slice(0, -1);
+      var _commits = '[' + _commits + ']';
+      var _commits = JSON.parse(_commits);
+      var _output = {
+        branch: b,
+        commits: _commits
+      }
+      output.push(_output);
+      if (i === branches.length - 1) {
+        res.json(output);
+      }
+    });
   }
 };
